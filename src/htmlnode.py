85cf0dc76c3f91adaafd46a7d68beca9377d726e
Leo-Mart <martin.christoffer.leo@gmail.com>
2025-11-19 18:56:32 +0100

    ParentNode class, unittests, moved LeafNode tests.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,7 +29,7 @@ class LeafNode(HTMLNode):
 
   def to_html(self):
     if self.value is None:
-      raise ValueError("Must contain a value!")
+      raise ValueError("invalid HTML: Must contain a value.")
     
     if self.tag is None:
       return self.value
@@ -37,4 +37,22 @@ class LeafNode(HTMLNode):
     return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
   
   def __repr__(self):
-    return f"LeafNode({self.tag, {self.value, {self.props}}})"
+    return f"LeafNode({self.tag}, {self.value}, {self.props})"
+  
+class ParentNode(HTMLNode):
+  def __init__(self, tag, children, props=None):
+    super().__init__(tag, None, children, props)
+
+  def to_html(self):
+    if self.tag is None:
+      raise ValueError("invalid HTML: must contain a tag.")
+    
+    if self.children is None:
+      raise ValueError("invalid HTML: must contain at least one child element.")
+    
+    children_html = ""
+
+    for child in self.children:
+      children_html += child.to_html()
+
+    return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
